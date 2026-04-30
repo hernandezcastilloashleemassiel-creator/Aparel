817ae20be48041d515c7874d4d3d7b8964a8ade1
Masiel05A <hernandezcastilloashleemassiel@gmail.com>
2026-04-30 16:10:04 -0600

    mi primer commit

---
+++ botones.js
@@ -0,0 +1,68 @@
+document.addEventListener("DOMContentLoaded", function(){
+
+    let productos = [];
+    document.getElementById("Montorecibido").addEventListener("input", function() {
+        
+        const montoPagar = parseFloat(document.getElementById("Montopagar").value) || 0;
+        const Montorecibido = parseFloat(this.value) || 0;
+
+        const cambio = Montorecibido - montoPagar;
+
+        document.getElementById("Cambio").value = cambio >= 0 
+            ? cambio.toFixed(2) 
+            : "0.00";
+    });
+
+    //  AGREGAR PRODUCTO
+    document.getElementById("btnagregarproducto").addEventListener("click", function(){
+
+        const nombre = document.getElementById("nombreProducto").value;
+        const cantidad = document.getElementById("cantidadProducto").value;
+        const precio = document.getElementById("precioProducto").value;
+
+        if(nombre === "" || cantidad === "" || precio === ""){
+            alert("Completa todos los campos");
+            return;
+        }
+
+        const total = cantidad * precio;
+
+        productos.push({nombre, cantidad, precio, total});
+
+        mostrarProductos();
+
+        document.getElementById("nombreProducto").value = "";
+        document.getElementById("cantidadProducto").value = "";
+        document.getElementById("precioProducto").value = "";
+    });
+
+    //MOSTRAR PRODUCTOS
+    function mostrarProductos(){
+        const tabla = document.getElementById("tabla-productos");
+        tabla.innerHTML = "";
+        let totalGeneral = 0;
+
+        productos.forEach((p, index) => {
+            const fila = document.createElement("tr");
+            totalGeneral += p.total;
+
+            fila.innerHTML = `
+                <td>${p.nombre}</td>
+                <td>${p.cantidad}</td>
+                <td>${p.precio}</td>
+                <td>${p.total}</td>
+                <td><button onclick="eliminarProducto(${index})">Eliminar</button></td>
+            `;
+
+            tabla.appendChild(fila);
+        });
+        document.getElementById("totalGeneral").textContent = totalGeneral;
+        document.getElementById("Montopagar").value = totalGeneral;
+    }
+
+    // ELIMINAR
+    window.eliminarProducto = function(index){
+        productos.splice(index, 1);
+        mostrarProductos();
+    }
+}); 
