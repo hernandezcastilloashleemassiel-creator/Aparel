28dc03d415db850f7d6fb9fceffae1f79ae3eee3
Masiel05A <hernandezcastilloashleemassiel@gmail.com>
2026-06-25 22:17:06 -0600

    Porfin

--- Admin/Proveedor/Proveedores.js
+++ Admin/Proveedor/Proveedores.js
@@ -1,23 +1,15 @@
 import { Proveedor } from "/Modelos/MProveedor.js";
 import { ProveedorService } from "/Service/SProveedor.js";
-
 let ProveedoresEditandoId = null;
 
 /* INFORMACIÓN DE BASE DE DATOS */
 async function cargarProveedores() {
-
     try {
-
         const Proveedores = await ProveedorService.obtenerTodos();
-
         const tabla = document.getElementById("tabla-Proveedor");
-
         tabla.innerHTML = "";
-
         Proveedores.forEach(Proveedores => {
-
             const fila = document.createElement("tr");
-
             fila.innerHTML = `
                 <td>${Proveedores.empresa}</td>
                 <td>${Proveedores.vendedor}</td>
@@ -35,125 +27,79 @@ async function cargarProveedores() {
                     </button>
                 </td>
             `;
-
             tabla.appendChild(fila);
-
         });
-
     } catch (error) {
-
         console.error("Error al cargar los proveedores:", error);
-
     }
 }
-
 /* ELIMINAR PROVEEDOR */
 async function eliminarProveedores(id) {
-
     if (!confirm("¿Deseas eliminar este Proveedor?")) {
         return;
     }
-
     try {
-
         const response = await ProveedorService.eliminar(id);
-
         if (response.ok) {
-
             alert("Proveedor eliminado correctamente");
-
             cargarProveedores();
-
         } else {
-
             alert("No se pudo eliminar el proveedor");
-
         }
-
     } catch (error) {
-
         console.error(error);
-
         alert("Error de conexión");
-
     }
-
 }
-
 /* AGREGAR O EDITAR PROVEEDOR */
 async function handleLoginSubmit(event) {
-
     event.preventDefault();
-
     const formData = new FormData(event.target);
-
     const DatosProveedor = new Proveedor(
-
         formData.get('NombreEmpresa'),
         formData.get('Vendedor'),
         formData.get('Telefono'),
         formData.get('Correo'),
         formData.get('Direccion'),
         formData.get('Estado') === 'true'
-
     );
 
     console.log("Proveedor:", DatosProveedor);
-
     try {
-
         const response = await ProveedorService.guardar(
             DatosProveedor,
             ProveedoresEditandoId
         );
-
         if (response.ok) {
-
             if (ProveedoresEditandoId) {
-
                 alert("Proveedor editado exitosamente");
-
             } else {
 
                 alert("Proveedor creado exitosamente");
 
             }
 
-            event.target.reset();
-
-            ProveedoresEditandoId = null;
-
+        event.target.reset();
+        ProveedoresEditandoId = null;
             document.getElementById("Modal").checked = false;
-
             cargarProveedores();
-
         } else {
-
             const error = await response.text();
-
             alert(error);
-
         }
 
     } catch (error) {
-
         console.error(error);
-
         alert("No se pudo conectar con el servidor.");
-
     }
 
 }
 
 /* EDITAR PROVEEDOR */
 async function editarProveedor(id) {
-
     try {
-
         const Proveedor = await ProveedorService.obtenerPorId(id);
-
         console.log(Proveedor);
-
         ProveedoresEditandoId = id;
 
         document.getElementById("NombreEmpresa").value = Proveedor.empresa;
@@ -162,26 +108,17 @@ async function editarProveedor(id) {
         document.getElementById("Correo").value = Proveedor.correo;
         document.getElementById("Direccion").value = Proveedor.direccion;
         document.getElementById("Estado").value = Proveedor.activo ? "true" : "false";
-
         document.getElementById("Modal").checked = true;
 
     } catch (error) {
-
         console.error(error);
-
         alert("Error al obtener proveedor");
-
     }
-
 }
-
 /* EVENTOS */
 document
     .getElementById("formProveedor")
     .addEventListener("submit", handleLoginSubmit);
-
 document.addEventListener("DOMContentLoaded", cargarProveedores);
-
-/* FUNCIONES GLOBALES */
 window.eliminarProveedores = eliminarProveedores;
 window.editarProveedor = editarProveedor;
--- Service/SProveedor.js
+++ Service/SProveedor.js
@@ -1,75 +1,45 @@
 const baseUrl = "https://localhost:7180/api";
 
 export const ProveedorService = {
-
-    // Obtener todos los proveedores
     async obtenerTodos() {
-
         const response = await fetch(`${baseUrl}/Proveedor`);
-
         if (!response.ok) {
             throw new Error("Error al obtener los Proveedores");
         }
-
         return await response.json();
-
     },
-
-    // Obtener proveedor por Id
+    // proveedor por Id
     async obtenerPorId(id) {
-
         const response = await fetch(`${baseUrl}/Proveedor/${id}`);
-
         if (!response.ok) {
             throw new Error(`Error ${response.status}`);
         }
-
         return await response.json();
-
     },
-
-    // Eliminar proveedor
+    // Eliminar 
     async eliminar(id) {
-
         return await fetch(`${baseUrl}/Proveedor/${id}`, {
-
             method: "DELETE"
-
         });
-
     },
-
-    // Guardar (Insertar o Editar)
+    // Insertar o Editar
     async guardar(proveedor, id = null) {
-
         if (id) {
-
             return await fetch(`${baseUrl}/Proveedor/${id}`, {
-
                 method: "PUT",
-
                 headers: {
                     "Content-Type": "application/json"
                 },
-
                 body: JSON.stringify(proveedor)
-
             });
-
         }
 
         return await fetch(`${baseUrl}/Proveedor`, {
-
             method: "POST",
-
             headers: {
                 "Content-Type": "application/json"
             },
-
             body: JSON.stringify(proveedor)
-
         });
-
     }
-
 };
